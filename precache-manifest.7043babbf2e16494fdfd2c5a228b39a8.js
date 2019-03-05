5cc34132f50e200b981e52351a679ba657858da6
Hieu Nguyen <hieunguyenel@gmail.com>
2019-03-05 11:52:00 +0700

    Updates

---
+++ precache-manifest.7043babbf2e16494fdfd2c5a228b39a8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b036c4feecb1a24d953",
+    "url": "/simple-magento-pwa/static/css/main.fa67bcdd.chunk.css"
+  },
+  {
+    "revision": "6b036c4feecb1a24d953",
+    "url": "/simple-magento-pwa/static/js/main.6b036c4f.chunk.js"
+  },
+  {
+    "revision": "36111805d123afe371ed",
+    "url": "/simple-magento-pwa/static/js/runtime~main.36111805.js"
+  },
+  {
+    "revision": "7e7edda9718f70b5d7df",
+    "url": "/simple-magento-pwa/static/css/2.87a7ee1e.chunk.css"
+  },
+  {
+    "revision": "7e7edda9718f70b5d7df",
+    "url": "/simple-magento-pwa/static/js/2.7e7edda9.chunk.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/simple-magento-pwa/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/simple-magento-pwa/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/simple-magento-pwa/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/simple-magento-pwa/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/simple-magento-pwa/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/simple-magento-pwa/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/simple-magento-pwa/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/simple-magento-pwa/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/simple-magento-pwa/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/simple-magento-pwa/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/simple-magento-pwa/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/simple-magento-pwa/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/simple-magento-pwa/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/simple-magento-pwa/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/simple-magento-pwa/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/simple-magento-pwa/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "7d4f819d2ab9c89ce98d592abb87a3fe",
+    "url": "/simple-magento-pwa/index.html"
+  }
+];
